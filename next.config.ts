f1543420930626b51c7899f43707411761ad30f4
Hriday Saikia <hridayanandasaikia96.com>
2026-01-09 14:08:07 +0530

    Update Next.js image config for Cloudinary

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,35 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//    images: {
+//     domains: ['res.cloudinary.com'], // ✅ allow Cloudinary
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-   images: {
-    domains: ['res.cloudinary.com'], // ✅ allow Cloudinary
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
 };
 
