6a15dc30b4ebfc0aaebb09d7edfd88a4c39a92ef
Hriday Saikia <hridayanandasaikia96.com>
2026-01-24 00:59:57 +0530

    update next.js to fix vulnerability

--- next.config.ts
+++ next.config.ts
@@ -17,9 +17,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   typescript: {
     ignoreBuildErrors: true,
   },
